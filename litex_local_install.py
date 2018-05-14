bbba2817b15b683eb5224b1009745d70f2ff1cd9
Florent Kermarrec <florent@enjoy-digital.fr>
2018-05-14 15:09:20 +0200

    litex_local_install: add litedram

--- litex_local_install.py
+++ litex_local_install.py
@@ -4,3 +4,8 @@ os.system("mv litex litex_tmp")
 os.system("mv litex_tmp/litex litex")
 os.system("rm -rf litex_tmp")
 os.system("cp litex/soc/tools/remote/litex_server.py litex_server.py")
+
+os.system("git clone http://github.com/enjoy-digital/litedram")
+os.system("mv litedram litedram_tmp")
+os.system("mv litedram_tmp/litedram litedram")
+os.system("rm -rf litedram_tmp")
