a03aaef40a6785ed48aea38b0cdee6cec4703a98
Florent Kermarrec <florent@enjoy-digital.fr>
2020-05-01 11:45:24 +0200

    lab004/firmware: update.

--- lab004/firmware/main.c
+++ lab004/firmware/main.c
@@ -7,17 +7,6 @@
 #include <console.h>
 #include <generated/csr.h>
 
-static void busy_wait(unsigned int ds)
-{
-	timer0_en_write(0);
-	timer0_reload_write(0);
-	timer0_load_write(CONFIG_CLOCK_FREQUENCY/10*ds);
-	timer0_en_write(1);
-	timer0_update_value_write(1);
-	while(timer0_value_read()) timer0_update_value_write(1);
-}
-
-
 static char *readstr(void)
 {
 	char c[2];
