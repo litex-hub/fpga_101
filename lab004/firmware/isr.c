31556cdaf7a8ab72cccc199610b5e5df0af383b3
Arnaud Durand <arnaud.durand@unifr.ch>
2020-05-13 02:33:14 +0200

    Init IRQ conditionally

--- lab004/firmware/isr.c
+++ lab004/firmware/isr.c
@@ -1,17 +1,26 @@
 #include <generated/csr.h>
+#include <generated/soc.h>
 #include <irq.h>
 #include <uart.h>
 
-extern void periodic_isr(void);
-
 void isr(void);
+
+#ifdef CONFIG_CPU_HAS_INTERRUPT
+
 void isr(void)
 {
-	unsigned int irqs;
+	__attribute__((unused)) unsigned int irqs;
 
 	irqs = irq_pending() & irq_getmask();
 
+#ifndef UART_POLLING
 	if(irqs & (1 << UART_INTERRUPT))
 		uart_isr();
-
+#endif
 }
+
+#else
+
+void isr(void){};
+
+#endif
